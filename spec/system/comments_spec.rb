ca8aae71b5c20130273aa8d3b88c8da3f607007e
kim-shun <kimura1417080@gmail.com>
2021-07-20 17:11:20 +0900

    ログインを試す

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :system do
+RSpec.describe 'コメント投稿', type: :system do
   before do
-    driven_by(:rack_test)
+    @tweet = FactoryBot.create(:tweet)
+    @comment = Faker::Lorem.sentence
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'ログインしたユーザーはツイート詳細ページでコメント投稿できる' do
+    visit new_user_session_path
+    fill_in 'Email', with: @tweet.user.email
+    fill_in 'Password', with: @tweet.user.password
+    binding.pry
+    find('input[name="commit"]').click
+  end
 end
